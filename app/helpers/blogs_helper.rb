936097d6e96ebc754ac4436651837657fa6d6d62
jbhatab <jbhatab@gmail.com>
2014-06-28 16:55:08 -0500

    cleaned up blog related form prompts in en yml file. got youtube helper working

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,6 +1,6 @@
 module BlogsHelper
 
   def youtube_video(youtube_id)
-    content_tag :iframe, height: '600', width: '100%', src: "//www.youtube.com/embed/" + youtube_id, frameborder: '0', allowfullscreen: true
+    content_tag :iframe, "", height: '600', width: '100%', src: "//www.youtube.com/embed/" + youtube_id, frameborder: '0', allowfullscreen: true
   end
 end
