a9df3c7e14777466c2292f4551aad84dbfcb365e
Scott Yu <scott.tj.yu@gmail.com>
2015-09-15 18:24:49 -0700

    added welcome msg to add profile skills

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912234032) do
+ActiveRecord::Schema.define(version: 20150915170020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20150912234032) do
     t.string   "admin"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.text     "about_me"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
