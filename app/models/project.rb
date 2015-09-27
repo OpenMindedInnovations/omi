41d237599af9eef69964deef9066e25e419a2c36
Scott Yu <scott.tj.yu@gmail.com>
2015-09-26 17:11:05 -0700

    added project owner to project upon project creation

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
 
   markable_as :favorite
 
-#  belongs_to :user
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :teams
   has_many :users, through: :teams
   accepts_nested_attributes_for :teams
