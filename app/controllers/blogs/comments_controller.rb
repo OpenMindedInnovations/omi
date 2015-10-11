0d438a2fb0482ee9843d511f1c5133be8600d6d8
jbhatab <jbhatab@gmail.com>
2015-10-11 12:07:06 -0500

    remove unecessary controller

--- app/controllers/blogs/comments_controller.rb
+++
@@ -1,9 +0,0 @@
-class Blogs::CommentsController < CommentsController
-  before_action :set_commentable
-
-  private
-
-    def set_commentable
-      @commentable = Blog.find(params[:blog_id])
-    end
-end
